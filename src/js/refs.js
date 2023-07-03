fc92231ca51aa82fc879d415ad29393a4ad6f0c1
maryna-haliieva <marina.galieva7@gmail.com>
2023-07-03 20:21:09 +0300

    Update

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  form: document.querySelector('.#search-form'),
+  input: document.querySelector('.input'),
+  galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
